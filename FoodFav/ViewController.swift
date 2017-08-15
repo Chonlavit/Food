268e91a76798510b21dcfdab494248f8b848feeb
iMac-52 <toek@imac-52s-mac-mini.local>
2017-08-15 17:33:47 +0700

    add sidebar / lib swReveal

--- FoodFav/ViewController.swift
+++ FoodFav/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var setting: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if revealViewController() != nil {
+            setting.target = self.revealViewController()
+            setting.action = #selector(SWRevealViewController.revealToggle(_:))
+        }
+        self.view.addGestureRecognizer(revealViewController().tapGestureRecognizer())
     }
 
     override func didReceiveMemoryWarning() {
